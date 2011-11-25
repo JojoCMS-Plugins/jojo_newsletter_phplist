1f2ed0d5c217ae4c28e8028125b589720fc11844
Anthony Spalding <antony@gardyneholt.co.nz>
2011-11-25 06:02:17 +0000

    Additions for viewing a newsletter online

--- jojo_newsletter.php
+++ jojo_newsletter.php
@@ -16,12 +16,12 @@
  * @link    http://www.jojocms.org JojoCMS
  */
 
-class Jojo_Plugin_Jojo_Newsletter
+class Jojo_Plugin_Jojo_Newsletter extends Jojo_Plugin
 {
     /**
      * Get all the bits of the news letter and put them all together
      */
-    public static function assembleNewsletter($id)
+    public static function assembleNewsletter($id, $online=false)
     {
         global $smarty;
         $result = array();
@@ -38,6 +38,7 @@ class Jojo_Plugin_Jojo_Newsletter
         $newsletter['outro'] = preg_replace('~^(&([a-zA-Z0-9]);)~',htmlentities('${1}'),$newsletter['outro']); 
 
         $smarty->assign('newsletter', $newsletter);
+        $smarty->assign('online', $online);
 
         /* Get the templateid */
         $templateid = $newsletter['template'];
@@ -46,7 +47,7 @@ class Jojo_Plugin_Jojo_Newsletter
             /* Get the image */
             $newsletter_header_image = Jojo::selectRow('SELECT bi_image FROM {bannerimage} WHERE bannerimageid = ?', $newsletter['header_image']);
             $imagelocation = _SITEURL . '/images/394x213/bannerimages/' . rawurlencode($newsletter_header_image['bi_image']);
-            $filename = Jojo_Plugin_Jojo_Newsletter::addTemplateImage($templateid, $imagelocation);
+            $filename = self::addTemplateImage($templateid, $imagelocation);
             $smarty->assign('bannerimage', $filename);
         }
 
@@ -76,7 +77,126 @@ class Jojo_Plugin_Jojo_Newsletter
         return $result;
     }
 
+    function _getContent()
+    {
+        global $smarty;
+        $content = array();
+
+        /* Do we have an id? */
+        $id = Jojo::getFormData('id',        0);
+        
+        if ($id) {
+            $newsletter = self::assembleNewsletter($id, $online=true);
+            $content['content'] = $newsletter['content'];
+            $content['title'] = $content['seotitle'] = $newsletter['subject'];
+            
+            header('Content-Type: text/html');
+            header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+            header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+            echo $newsletter;
+            exit;
+
+        } else {
+            $newsletters = Jojo::selectQuery('SELECT * FROM {newsletter} ORDER BY date DESC');
+            foreach ($newsletters as &$i) {
+             $i['title']   = htmlspecialchars($i['name'], ENT_COMPAT, 'UTF-8', false);
+              $i['datefriendly'] = Jojo::formatTimestamp($i['date'], "medium");
+            }
+            $smarty->assign('newsletters', $newsletters);
+        
+            $content['content'] = $smarty->fetch('jojo_newsletter_index.tpl');
+        }
+        
+        return $content;
+    }
+
+    function getCorrectUrl()
+    {
+        $id = Jojo::getFormData('id', 0);
+        $correcturl = self::_getPrefix() . '/' . $id . '/';
+
+        if ($id && $correcturl) {
+            return _SITEURL . '/' . $correcturl;
+        }
+
+        /* default */
+        return parent::getCorrectUrl();
+    }
+
+    static public function isUrl($uri)
+    {
+        $prefix = false;
+        $getvars = array();
+        /* Check for standard plugin url format matches */
+        if ($uribits = parent::isPluginUrl($uri)) {
+            $prefix = $uribits['prefix'];
+            $getvars = $uribits['getvars'];
+        } else {
+            return false;
+        }
+        /* Check the prefix matches */
+        if ($res = self::checkPrefix($prefix)) {
+            /* If full uri matches a prefix it's an index page so ignore it and let the page plugin handle it */
+            if (self::checkPrefix(trim($uri, '/'))) {
+                return false;
+            }
+
+            /* The prefix is good, pass through uri parts */
+            foreach($getvars as $k => $v) {
+                $_GET[$k] = $v;
+            }
+            return true;
+        }
+        return false;
+    }
+
     /**
+     * Check if a prefix is a prefix for this plugin
+     */
+    static public function checkPrefix($prefix)
+    {
+        static $_prefixes;
+        if (!isset($_prefixes)) {
+            /* Initialise cache */
+            $_prefixes = array();
+        }
+        /* Check if it's in the cache */
+        if (isset($_prefixes[$prefix])) {
+            return $_prefixes[$prefix];
+        }
+        /* Check everything */
+        $testPrefix = self::_getPrefix();
+        $_prefixes[$testPrefix] = true;
+        if ($testPrefix == $prefix) {
+            /* The prefix is good */
+            return true;
+        }
+        /* Didn't match */
+        $_prefixes[$testPrefix] = false;
+        return false;
+    }
+
+    static function _getPrefix()
+    {
+        $cacheKey = 'newsletterview';
+
+        /* Have we got a cached result? */
+        static $_cache;
+        if (isset($_cache[$cacheKey])) {
+            return $_cache[$cacheKey];
+        }
+
+        /* Cache some stuff */
+        $res = Jojo::selectRow("SELECT p.pageid, pg_title, pg_url FROM {page} p WHERE `pg_link` = 'jojo_plugin_jojo_newsletter'");
+        if ($res) {
+            $_cache[$cacheKey] = !empty($res['pg_url']) ? $res['pg_url'] : $res['pageid'] . '/' . Jojo::clean($res['pg_title']);
+        } else {
+            $_cache[$cacheKey] = '';
+        }
+        return $_cache[$cacheKey];
+    }
+
+   /**
      * Add an image file to the template images table and return the filename
      * for the image
      */
